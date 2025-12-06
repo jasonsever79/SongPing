d5bd97e6350f0de815c0da16291226771de6e7e9
jasonsever79 <newjasonsever@gmail.com>
2025-12-06 13:41:00 -0600

    Update page.tsx

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -6,25 +6,28 @@ export default function SettingsPage() {
 
   return (
 
-    <div style={{ padding: 30 }}>
+    <div style={{ padding: 20 }}>
 
       <h1>Settings</h1>
 
       <p>Customize your SongPing experience.</p>
+
+
+
+      <div style={{ marginTop: 40 }}>
+
+        <a href="/" style={{ display: "block", marginBottom: 10 }}>Home</a>
+
+        <a href="/send" style={{ display: "block", marginBottom: 10 }}>Send Song</a>
+
+        <a href="/inbox" style={{ display: "block", marginBottom: 10 }}>Inbox</a>
+
+        <a href="/settings" style={{ display: "block", marginBottom: 10 }}>Settings</a>
+
+      </div>
 
     </div>
 
   );
 
 }
-<div style={{ marginTop: 40 }}>
-
-  <a href="/" style={{ display: "block", marginBottom: 10 }}>Home</a>
-
-  <a href="/send" style={{ display: "block", marginBottom: 10 }}>Send Song</a>
-
-  <a href="/inbox" style={{ display: "block", marginBottom: 10 }}>Inbox</a>
-
-  <a href="/settings" style={{ display: "block", marginBottom: 10 }}>Settings</a>
-
-</div>
