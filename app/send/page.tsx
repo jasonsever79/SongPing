7162e40571955db6b87ab3830e481f4ed568f9f3
jasonsever79 <newjasonsever@gmail.com>
2025-12-06 05:19:55 -0600

    Create page.tsx

---
+++ app/send/page.tsx
@@ -0,0 +1,39 @@
+export default function SendSong() {
+
+  return (
+
+    <div style={{ padding: 30 }}>
+
+      <h1>Send a Song</h1>
+
+      <p>This is where you will choose a song from your phone and send it to your partner.</p>
+
+
+
+      <div style={{ marginTop: 20 }}>
+
+        <button
+
+          style={{
+
+            padding: "10px 20px",
+
+            fontSize: "16px",
+
+            cursor: "pointer"
+
+          }}
+
+        >
+
+          Choose Song
+
+        </button>
+
+      </div>
+
+    </div>
+
+  );
+
+}
