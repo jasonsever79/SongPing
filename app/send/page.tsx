5c424d5e23c8707e25680a86ab1fb573573d8938
jasonsever79 <newjasonsever@gmail.com>
2025-12-06 06:54:49 -0600

    Update page.tsx

--- app/send/page.tsx
+++ app/send/page.tsx
@@ -187,3 +187,14 @@ export default function SendSong() {
   );
 
 }
+<div style={{ marginTop: 40 }}>
+
+  <a href="/" style={{ display: "block", marginBottom: 10 }}>Home</a>
+
+  <a href="/send" style={{ display: "block", marginBottom: 10 }}>Send Song</a>
+
+  <a href="/inbox" style={{ display: "block", marginBottom: 10 }}>Inbox</a>
+
+  <a href="/settings" style={{ display: "block", marginBottom: 10 }}>Settings</a>
+
+</div>
