387dbbded986a82feda519249e8af82ea23352ec
jasonsever79 <newjasonsever@gmail.com>
2025-12-06 13:59:23 -0600

    Update page.tsx

--- app/inbox/page.tsx
+++ app/inbox/page.tsx
@@ -1,7 +1,3 @@
-"use client";
-
-
-
 export default function InboxPage() {
 
   return (
@@ -12,19 +8,38 @@ export default function InboxPage() {
 
       <p>Your received songs will appear here.</p>
 
+
+
+      <div style={{ marginTop: 40 }}>
+
+        <a href="/" style={{ display: "block", marginBottom: 10 }}>
+
+          Home
+
+        </a>
+
+        <a href="/send" style={{ display: "block", marginBottom: 10 }}>
+
+          Send Song
+
+        </a>
+
+        <a href="/inbox" style={{ display: "block", marginBottom: 10 }}>
+
+          Inbox
+
+        </a>
+
+        <a href="/settings" style={{ display: "block", marginBottom: 10 }}>
+
+          Settings
+
+        </a>
+
+      </div>
+
     </div>
 
   );
 
 }
-<div style={{ marginTop: 40 }}>
-
-  <a href="/" style={{ display: "block", marginBottom: 10 }}>Home</a>
-
-  <a href="/send" style={{ display: "block", marginBottom: 10 }}>Send Song</a>
-
-  <a href="/inbox" style={{ display: "block", marginBottom: 10 }}>Inbox</a>
-
-  <a href="/settings" style={{ display: "block", marginBottom: 10 }}>Settings</a>
-
-</div>
