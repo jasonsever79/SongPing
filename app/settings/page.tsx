91c5da57ecaef44d2271a88a3e366b39bd191c16
jasonsever79 <newjasonsever@gmail.com>
2025-12-06 06:48:17 -0600

    Create page.tsx

---
+++ app/settings/page.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+
+
+export default function SettingsPage() {
+
+  return (
+
+    <div style={{ padding: 30 }}>
+
+      <h1>Settings</h1>
+
+      <p>Customize your SongPing experience.</p>
+
+    </div>
+
+  );
+
+}
